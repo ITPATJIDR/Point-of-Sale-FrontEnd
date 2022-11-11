4d596c73cbfad7137c5a77a8c92c868ad17a4c60
ITPATJIDR <ittipatjitrada@gmail.com>
2022-11-11 23:01:15 +0700

    add choose extra menu

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useAuth} from "../../context/AuthContext"
 import LoginPage from '../../Components/LoginPage';
 import Sidebar from '../../Components/Parent';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 
+const MenuList = ({item,setChooseMenu}) =>{
+	return(
+		<div className="Main-Menu-List">
+			{item.ListMenu.map((item,i)=>{
+				return(
+					<div key={i} className="ExtraMenu-List" onClick={(e) => setChooseMenu(item.Name)}>{item.Name}</div>
+				)
+			})}
+		</div>
+	)
+}
 
 const Menu = ({menu}) =>{
+
 	const {auth,setAuth} = useAuth();
+	const [chooseMenu,setChooseMenu] = useState();
+	const [toggle,setToggle] = useState(false);
+	const toggleChecked = () => setToggle(value => !value);
+
 	return (
 		<div>
 			{auth ? 
@@ -17,22 +35,30 @@ const Menu = ({menu}) =>{
 						<div className="Choose-Menu-Name">
 							<p>{menu.MenuName}</p>
 						</div>
-					</div>
+					</div> 
 					<div className="Choose-ExtraMenu" >
-						{menu.ExtraMenu.map((item,i)=>{
-							return(
-								<div key={i} className="ExtraMenuName">
-									<p>{item.ExtraMenuName}</p>
-									{item.ListMenu.map((item,i)=>{
-										return (
-											<div key={i}>
-												<h2 >{item.Name}</h2>
+						<div className="Render-ExtraMenu"> 
+							{menu.ExtraMenu.map((item,i)=>{
+								return(
+									<div key={i} className="ExtraMenuName">
+										<p>{item.ExtraMenuName}</p>
+										<div className="ListMenu">
+											<div className="ListMenu-Choose">
+												<p>{chooseMenu}</p>
 											</div>
-										)
-									})}
-								</div>
-							)
-						})}
+											<div className="ListMenu-Button" onClick={toggleChecked}>
+												{toggle 
+												? <FontAwesomeIcon icon={faCaretUp} style={{width:50,height:50}} color="#730303" /> 
+												: <FontAwesomeIcon icon={faCaretDown} style={{width:50,height:50}} color="#730303" />
+												}
+											</div>
+										</div>
+										{toggle ?  <MenuList item={item} setChooseMenu={setChooseMenu}/> : null}
+									</div>
+								)
+							})}
+							<button type="Submit" className="Choose-ExtraMenu-Button">Submit</button>
+						</div>
 					</div>
 				</div>
 			</Sidebar>
